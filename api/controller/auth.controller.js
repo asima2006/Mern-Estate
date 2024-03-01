2f76ff86599f387b1453b5f976e28217f7f11117
Mohit Singh Raghav <118093505+asima2006@users.noreply.github.com>
2024-03-02 03:01:04 +0530

    Sign Up page + error handling

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,7 @@
 const User = require('../model/user.modal.js');
 const bcrypt = require('bcryptjs');
 
-const signUp = async (req,res) => {
+const signUp = async (req,res,next) => {
     const {username, email, password} = req.body;
     const hashedPassword = bcrypt.hashSync(password,10);
     const newUser = new User({username, email, password: hashedPassword});
@@ -9,7 +9,7 @@ const signUp = async (req,res) => {
         await newUser.save();
         res.status(201).json("User created successfully");
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 }
 
