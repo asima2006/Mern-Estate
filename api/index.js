86db80ea15740f69d6a3bde8a0adf5bf236b3f16
Mohit Singh Raghav <118093505+asima2006@users.noreply.github.com>
2024-03-02 01:11:12 +0530

    Creating signUp api and testing

--- api/index.js
+++ api/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config()
+const authRouter = require('./routes/auth.routes.js')
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to MongoDB");
 }).catch(err => console.log(err));
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log('Server is listening on 3000');
 });
+
+app.use('/api/auth',authRouter);
