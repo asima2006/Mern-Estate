bc41ed402cd5c990062a9558c2d84ce98602d730
Mohit Singh Raghav <118093505+asima2006@users.noreply.github.com>
2024-03-01 05:31:31 +0530

    Database Setup

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch(err => console.log(err));
 
 const app = express();
 
